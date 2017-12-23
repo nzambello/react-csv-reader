c932735ba449cf6e6d71d97d081b3e37180990a8
nzambello <nicola.zambello@redturtle.it>
2017-12-23 16:51:49 +0100

    added component and basic functionalities

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from "react"
-import { func } from "prop-types"
+import React from 'react';
+import { string, func } from 'prop-types';
+const PapaParse = require('papaparse/papaparse.min.js');
 
-const CSVReader = ({ onFileLoaded, onError }) => {
+const CSVReader = ({ cssClass, onFileLoaded, onError }) => {
+  let fileContent = undefined;
+
+  const handleChangeFile = e => {
+    let reader = new FileReader();
+
+    reader.onload = event => {
+      const csvData = PapaParse.parse(event.target.result, {
+        error: onError
+      });
+      onFileLoaded(csvData.data);
+    };
+
+    reader.readAsText(e.target.files[0]);
+  };
+
   return (
-    <div>
-      <p>Welcome to CSVReader</p>
-    </div>
-  )
-}
+    <input
+      className={cssClass ? cssClass : ''}
+      type="file"
+      id="fileElem"
+      accept="text/csv"
+      onChange={e => handleChangeFile(e)}
+    />
+  );
+};
 
 CSVReader.propTypes = {
+  cssClass: string,
   onFileLoaded: func,
   onError: func
-}
+};
 
-export default CSVReader
+export default CSVReader;
