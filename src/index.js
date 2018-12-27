2d3f90b50fab73525d3a45c3863afaa26e9d789e
Nicola Zambello <nzambello@protonmail.com>
2018-12-28 00:12:49 +0100

    Fixing `accept` file type in input (#11)
    fixing accept file type

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const CSVReader = ({
         type="file"
         id={inputId}
         style={inputStyle}
-        accept="text/csv"
+        accept=".csv, text/csv"
         onChange={e => handleChangeFile(e)}
       />
     </div>
