6c3d2a96ff7bc2a20efa72e981651ace2b0bac0e
Nicola Zambello <nzambello@protonmail.com>
2020-03-16 17:32:22 +0100

    added a11y axe tests, refactoring tests

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,47 +1,50 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
 import { cleanup, render } from '@testing-library/react'
+import { axe, toHaveNoViolations } from 'jest-axe'
 
 import CSVReader from './index'
 
+expect.extend(toHaveNoViolations)
 afterEach(cleanup)
 
-test('Renders basic CSVReader', () => {
-  const component = renderer.create(<CSVReader onFileLoaded={() => {}} />)
+const papaparseOptions = {
+  header: true,
+  dynamicTyping: true,
+  skipEmptyLines: true,
+  transformHeader: (header: string) => header.toLowerCase().replace(/\W/g, '_'),
+}
+
+const csvReader = (
+  <CSVReader
+    accept=".csv, text/csv, .tsv, test/tsv"
+    cssClass="custom-csv-reader"
+    cssInputClass="custom-csv-input"
+    fileEncoding="iso-8859-1"
+    inputId="react-csv-reader"
+    inputStyle={{ color: 'red' }}
+    label="CSV input label text"
+    onError={e => console.error(e)}
+    onFileLoaded={(data, fileInfo) => console.dir(data, fileInfo)}
+    parserOptions={papaparseOptions}
+    disabled
+  />
+)
+
+test('CSVReader is being rendered', () => {
+  const component = renderer.create(csvReader)
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
 
-describe('CSVReader with all custom props', () => {
-  const papaparseOptions = {
-    header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true,
-    transformHeader: (header: string) => header.toLowerCase().replace(/\W/g, '_'),
-  }
+test('CSVReader is accessible', async () => {
+  const { container } = render(csvReader)
+  const results = await axe(container)
 
-  const csvReader = (
-    <CSVReader
-      accept=".csv, text/csv, .tsv, test/tsv"
-      cssClass="custom-csv-reader"
-      cssInputClass="custom-csv-input"
-      fileEncoding="iso-8859-1"
-      inputId="react-csv-reader"
-      inputStyle={{ color: 'red' }}
-      label="CSV input label text"
-      onError={e => console.error(e)}
-      onFileLoaded={(data, fileInfo) => console.dir(data, fileInfo)}
-      parserOptions={papaparseOptions}
-      disabled
-    />
-  )
+  expect(results).toHaveNoViolations()
+})
 
-  test('is being rendered', () => {
-    const component = renderer.create(csvReader)
-    let tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-
+describe('Testing CSVReader props:', () => {
   test('has accept prop set', () => {
     const accept = '.csv, text/csv, .tsv, test/tsv'
     const { getByLabelText } = render(csvReader)
