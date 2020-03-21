033c57a02e015914d8852076c942fabf96c810e6
Nicola Zambello <nzambello@protonmail.com>
2020-03-21 09:58:07 +0100

    added declaration file

---
+++ dist/index.d.ts
@@ -0,0 +1,21 @@
+import * as React from 'react';
+export interface IFileInfo {
+    name: string;
+    size: number;
+    type: string;
+}
+export interface CSVReaderProps {
+    accept?: string;
+    cssClass?: string;
+    cssInputClass?: string;
+    fileEncoding?: string;
+    inputId?: string;
+    inputStyle?: object;
+    label?: string | React.ReactNode;
+    onError?: (error: Error) => void;
+    onFileLoaded: (data: Array<any>, fileInfo: IFileInfo) => any;
+    parserOptions?: object;
+    disabled?: boolean;
+}
+declare const CSVReader: React.FC<CSVReaderProps>;
+export default CSVReader;
